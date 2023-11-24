0d6d3f92a22d5e379fced25f4ae9f13a8f27ad68
RSjairo <jsonico@jairosolutions.com>
2023-11-24 16:32:36 +0800

    initial

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+// using API.Helpers;
+
+namespace API.Controllers
+{
+    // [ServiceFilter(typeof(LogUserActivity))]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
